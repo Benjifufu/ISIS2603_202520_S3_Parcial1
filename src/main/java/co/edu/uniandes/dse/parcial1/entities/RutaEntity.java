d03b71afcdba612d287dba51004c1e368f238b09
Benjifufu <b.perez11@uniandes.edu.co>
2025-09-17 08:23:36 -0500

    persistencia

--- src/main/java/co/edu/uniandes/dse/parcial1/entities/RutaEntity.java
+++ src/main/java/co/edu/uniandes/dse/parcial1/entities/RutaEntity.java
@@ -1,7 +1,13 @@
 package co.edu.uniandes.dse.parcial1.entities;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
 import lombok.Data;
+import uk.co.jemos.podam.common.PodamExclude;
+
 
 @Data
 @Entity
@@ -10,5 +16,9 @@ public class RutaEntity extends BaseEntity {
     private String nombre;
     private String color;
     private String tipo;
+
+    @PodamExclude
+    @ManyToMany
+    private Collection<EstacionEntity> estacion = new ArrayList<>();
 
 }
---
+++ src/main/java/co/edu/uniandes/dse/parcial1/services/EstacionService.java
@@ -0,0 +1,13 @@
+package co.edu.uniandes.dse.parcial1.services;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class EstacionService {
+
+    
+    
+}
